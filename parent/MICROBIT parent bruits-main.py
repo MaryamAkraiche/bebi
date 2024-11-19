071eadbdc18551980e1a75eb88571ce8642aa265
pMaurice <maurice.pinnoy@student.uclouvain.be>
2024-11-19 10:24:39 +0100

    bruits de fonds ajoutés

---
+++ parent/MICROBIT parent bruits-main.py
@@ -0,0 +1,37 @@
+# Imports go at the top
+from microbit import *
+import music
+import radio
+
+radio.on()
+radio.config(group = 3)
+
+# Code in a 'while True:' loop repeats forever
+while True:
+    if radio.receive() == "Attention, bebe pleure !":
+        audio.play(Sound.MYSTERIOUS)
+        display.scroll("Bebe pleure !")
+        if button_a.was_pressed():
+            display.scroll("mode musique choisi")
+            sleep(100)
+            display.scroll("vous confirmez votre choix ?")
+            if button_a.was_pressed():
+                display.scroll("mode musique active")
+                radio.send("musique")
+            elif button_b.was_pressed():
+                pass          
+        elif button_b.was_pressed():
+            display.scroll("bruits de fonds choisi")
+            sleep(100)
+            display.scroll("vous confirmez votre choix ?")
+            if button_a.was_pressed():
+                display.scroll("mode bruits de fonds active")
+                radio.send("bruits")
+            elif button_b.was_pressed():
+                pass  
+        elif pin_logo.is_touched():
+            display.scroll("ne rien faire")
+            radio.send("rien")
+            
+            
+        
