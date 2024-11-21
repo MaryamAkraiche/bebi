1764127434336a88122ff4b235fd499a64e7a7a8
Maryam Akraiche <165012774+MaryamAkraiche@users.noreply.github.com>
2024-11-21 17:02:54 +0100

    Update parent.py
    added code for qunatité de lait

--- parent.py
+++ parent.py
@@ -1,19 +1,17 @@
-# Imports go at the top
 from microbit import *
-import radio
-import music
 
-display.show(Image.DUCK)
-music.play(music.JUMP_UP)
-Lait = 0
+milk_doses = 0
 while True:
-    if button_a.was_pressed() == 1:
-        display.scroll('Mode Lait Active', delay= 100)
+    if button_a.is_pressed() and button_b.is_pressed():
+        milk_doses = 0  
+        display.show("0") 
+        sleep(500) 
+    elif button_a.is_pressed():
+        milk_doses += 1 
+        display.show(str(milk_doses))
         sleep(500)
-        display.scroll("Rester appuyer pendant L'Allaitement", delay= 75)
-        while button_a.is_pressed:
-            display.scroll("Alaitement en cours")
-            if button_a.is_pressed() is False:
-                continue
-        Lait += 1
-        display.scroll(Lait)
+    elif button_b.is_pressed():
+        if milk_doses > 0:
+            milk_doses -= 1  
+        display.show(str(milk_doses))
+        sleep(500)
